84255f39aa3032e2c8db61de2fc57b7ae9af3978
Genilton Souza <genilton.souza26@gmail.com>
2021-05-14 01:16:15 -0400

    Criando evento do formulário
    aula 2-04 módulo: Angular-comecando-framework

--- bytebank/src/app/nova-transferencia/nova-transferencia.component.ts
+++ bytebank/src/app/nova-transferencia/nova-transferencia.component.ts
@@ -3,13 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-nova-transferencia',
   templateUrl: './nova-transferencia.component.html',
-  styleUrls: ['./nova-transferencia.component.scss']
+  styleUrls: ['./nova-transferencia.component.scss'],
 })
 export class NovaTransferenciaComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  transferir() {
+    console.log('transferindo...');
+    //console.log(form);
   }
-
 }
