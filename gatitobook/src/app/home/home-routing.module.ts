33b874ccfb7a87359a34b3865e191fb85417eab8
Genilton Souza <genilton.souza26@gmail.com>
2021-07-26 15:51:40 -0400

    Aula 10: Login
    Curso Angular: boas práticas em arquitetura e formulários

--- gatitobook/src/app/home/home-routing.module.ts
+++ gatitobook/src/app/home/home-routing.module.ts
@@ -1,3 +1,4 @@
+import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home.component';
@@ -6,6 +7,7 @@ const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+    children: [{ path: '', component: LoginComponent }],
   },
 ];
 
