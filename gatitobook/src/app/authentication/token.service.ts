3b6ce77875d8547d43d524aca7a8f3ff65365ae5
Genilton Souza <genilton.souza26@gmail.com>
2021-08-02 15:02:49 -0400

    05 Autenticação- Aula 03: Token
    Curso Angular: boas práticas em arquitetura e formulários. Módulo 5

---
+++ gatitobook/src/app/authentication/token.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+
+const KEY = 'token';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TokenService {
+  returnToken() {
+    return localStorage.getItem(KEY) ?? '';
+  }
+  saveToken(token: string) {
+    localStorage.setItem(KEY, token);
+  }
+  deleteToken() {
+    localStorage.removeItem(KEY);
+  }
+  hasToken() {
+    return !!this.returnToken();
+  }
+}
