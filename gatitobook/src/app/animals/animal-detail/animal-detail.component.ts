2a4fc9f3c57dbf446d56703985cae70b75ad2539
Genilton Souza <genilton.souza26@gmail.com>
2021-08-10 17:57:16 -0400

    03 Detalhe do animal- Aula 07: Curtir e excluir foto
    Curso Angular: Controle o fluxo de navegação. Módulo 3

--- gatitobook/src/app/animals/animal-detail/animal-detail.component.ts
+++ gatitobook/src/app/animals/animal-detail/animal-detail.component.ts
@@ -2,7 +2,7 @@ import { AnimalsService } from './../animals.service';
 import { Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
 import { Animal } from '../animals';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-animal-detail',
@@ -15,11 +15,31 @@ export class AnimalDetailComponent implements OnInit {
 
   constructor(
     private animalsService: AnimalsService,
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
     this.animalId = this.activatedRoute.snapshot.params.animalId;
     this.animal$ = this.animalsService.findByID(this.animalId);
+  }
+
+  like() {
+    this.animalsService.like(this.animalId).subscribe((like) => {
+      if (like) {
+        this.animal$ = this.animalsService.findByID(this.animalId);
+      }
+    });
+  }
+
+  delete() {
+    this.animalsService.deleteAnimal(this.animalId).subscribe(
+      () => {
+        this.router.navigate(['/animais/']);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
 }
--- gatitobook/src/app/animals/animals.service.ts
+++ gatitobook/src/app/animals/animals.service.ts
@@ -28,7 +28,7 @@ export class AnimalsService {
 
   like(id: number): Observable<boolean> {
     return this.http
-      .post(`${API}/photos/${id}/likes`, {}, { observe: 'response' })
+      .post(`${API}/photos/${id}/like`, {}, { observe: 'response' })
       .pipe(
         mapTo(true),
         catchError((error) => {
