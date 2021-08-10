5baf31a884d0e376c002e6390bbc56e4447e7e02
Genilton Souza <genilton.souza26@gmail.com>
2021-08-10 17:32:51 -0400

    03 Detalhe do animal- Aula 06: Métodos de curtir e excluir foto
    Curso Angular: Controle o fluxo de navegação. Módulo  3

--- gatitobook/src/app/animals/animals.service.ts
+++ gatitobook/src/app/animals/animals.service.ts
@@ -1,12 +1,13 @@
 import { environment } from './../../environments/environment';
 import { TokenService } from './../authentication/token.service';
-import { Observable } from 'rxjs';
+import { Observable, of, throwError } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Animal, Animals } from './animals';
+import { catchError, mapTo } from 'rxjs/operators';
 
 const API = environment.apiUrl;
-
+const NOT_MODIFIED = '304';
 @Injectable({
   providedIn: 'root',
 })
@@ -19,5 +20,20 @@ export class AnimalsService {
 
   findByID(id: number): Observable<Animal> {
     return this.http.get<Animal>(`${API}/photos/${id}`);
+  }
+
+  deleteAnimal(id: number): Observable<Animal> {
+    return this.http.delete<Animal>(`${API}/photos/${id}`);
+  }
+
+  like(id: number): Observable<boolean> {
+    return this.http
+      .post(`${API}/photos/${id}/likes`, {}, { observe: 'response' })
+      .pipe(
+        mapTo(true),
+        catchError((error) => {
+          return error.status === NOT_MODIFIED ? of(false) : throwError(error);
+        })
+      );
   }
 }
