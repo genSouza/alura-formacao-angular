cc1367e5c1056a1d4ac3a9fe776b5223043ec130
Genilton Souza <genilton.souza26@gmail.com>
2021-08-10 11:19:35 -0400

    02 Lista de animais- Aula 03: Lista de animais
    Curso Angular: Controle o fluxo de navegação. Módulo 2

--- gatitobook/src/app/animals/animals-list/animals-list.component.ts
+++ gatitobook/src/app/animals/animals-list/animals-list.component.ts
@@ -1,15 +1,27 @@
+import { AnimalsService } from './../animals.service';
+import { UserService } from './../../authentication/user/user.service';
+import { Animals } from './../animals';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-animals-list',
   templateUrl: './animals-list.component.html',
-  styleUrls: ['./animals-list.component.css']
+  styleUrls: ['./animals-list.component.css'],
 })
 export class AnimalsListComponent implements OnInit {
+  animals!: Animals;
 
-  constructor() { }
+  constructor(
+    private userService: UserService,
+    private animalsService: AnimalsService
+  ) {}
 
   ngOnInit(): void {
+    this.userService.getUser().subscribe((user) => {
+      const userName = user.name ?? '';
+      this.animalsService.userList(userName).subscribe((animals) => {
+        this.animals = animals;
+      });
+    });
   }
-
 }
