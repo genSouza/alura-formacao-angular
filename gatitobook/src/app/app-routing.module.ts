641aa8fbe68cf32c6f80b43f5a3adafa3cc54d42
Genilton Souza <genilton.souza26@gmail.com>
2021-08-10 13:49:08 -0400

    02 Lista de animais- Aula 06: Associando a Rota
    Curso Angular: Controle o fluxo de navegação. Módulo 2

--- gatitobook/src/app/app-routing.module.ts
+++ gatitobook/src/app/app-routing.module.ts
@@ -1,16 +1,20 @@
+import { LoginGuard } from './authentication/login.guard';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthenticationGuard } from './authentication/authentication.guard';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   {
     path: 'home',
     loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
+    canLoad: [LoginGuard]
   },
   {
     path: 'animais',
     loadChildren: () =>
       import('./animals/animals.module').then((m) => m.AnimalsModule),
+      canLoad: [AuthenticationGuard],
   },
 ];
 
