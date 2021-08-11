f7234b33a87605db2668e43fe426f5b6311c8dc0
Genilton Souza <genilton.souza26@gmail.com>
2021-08-11 15:41:43 -0400

    04 Comentários- Aula 07: Organização dos módulos
    Curso Angular: Controle o fluxo de navegação. Módulo 4

--- gatitobook/src/app/animals/animals.module.ts
+++ gatitobook/src/app/animals/animals.module.ts
@@ -1,16 +1,13 @@
-import { MessageModule } from './../components/message/message.module';
+import { SharedModule } from './../shared/shared.module';
 import { CardModule } from './../components/card/card.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { AnimalsRoutingModule } from './animals-routing.module';
 import { AnimalsListComponent } from './animals-list/animals-list.component';
 import { AnimalComponent } from './animal/animal.component';
 import { GridAnimalsPhotoComponent } from './grid-animals-photo/grid-animals-photo.component';
 import { AnimalDetailComponent } from './animal-detail/animal-detail.component';
 import { CommentsComponent } from './animal-detail/comments/comments.component';
-import { ReactiveFormsModule } from '@angular/forms';
-
 @NgModule({
   declarations: [
     AnimalsListComponent,
@@ -19,12 +16,6 @@ import { ReactiveFormsModule } from '@angular/forms';
     AnimalDetailComponent,
     CommentsComponent,
   ],
-  imports: [
-    CommonModule,
-    AnimalsRoutingModule,
-    CardModule,
-    MessageModule,
-    ReactiveFormsModule,
-  ],
+  imports: [CommonModule, AnimalsRoutingModule, CardModule, SharedModule],
 })
 export class AnimalsModule {}
--- gatitobook/src/app/home/home.module.ts
+++ gatitobook/src/app/home/home.module.ts
@@ -1,27 +1,15 @@
-import { MessageModule } from './../components/message/message.module';
+import { SharedModule } from './../shared/shared.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { LoginComponent } from './login/login.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { NewUserComponent } from './new-user/new-user.component';
 
-
 @NgModule({
-  declarations: [
-    HomeComponent,
-    LoginComponent,
-    NewUserComponent
-  ],
-  imports: [
-    CommonModule,
-    HomeRoutingModule,
-    FormsModule,
-    ReactiveFormsModule,
-    MessageModule
-  ],
-  exports: [HomeComponent]
+  declarations: [HomeComponent, LoginComponent, NewUserComponent],
+  imports: [CommonModule, HomeRoutingModule, FormsModule, SharedModule],
+  exports: [HomeComponent],
 })
-export class HomeModule { }
+export class HomeModule {}
