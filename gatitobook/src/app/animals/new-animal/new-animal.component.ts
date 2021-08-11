e23b49587eabeaed40e758958cf4ac43f73eec81
Genilton Souza <genilton.souza26@gmail.com>
2021-08-11 17:11:27 -0400

    05 Novo Animal- Aula 05: Campo de preview
    Curso Angular: Controle o fluxo de navegação. Módulo 5

--- gatitobook/src/app/animals/new-animal/new-animal.component.ts
+++ gatitobook/src/app/animals/new-animal/new-animal.component.ts
@@ -1,15 +1,24 @@
+import { FormGroup } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-new-animal',
   templateUrl: './new-animal.component.html',
-  styleUrls: ['./new-animal.component.css']
+  styleUrls: ['./new-animal.component.css'],
 })
 export class NewAnimalComponent implements OnInit {
+  animalForm!: FormGroup;
+  file!: File;
+  preview!: string;
+  uploadComplete = 0;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  upload() {
+    console.log('foo');
   }
 
+  saveFile(file: any) {}
 }
