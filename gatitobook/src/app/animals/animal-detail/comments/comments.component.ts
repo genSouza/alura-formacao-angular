78e0d96e78f7cebff3475ced479d248d5f697f2d
Genilton Souza <genilton.souza26@gmail.com>
2021-08-11 14:31:52 -0400

    04 Comentários- Aula 04: Componente
    Curso Angular: Controle o fluxo de navegação. Módulo 4

--- gatitobook/src/app/animals/animal-detail/comments/comments.component.ts
+++ gatitobook/src/app/animals/animal-detail/comments/comments.component.ts
@@ -1,5 +1,8 @@
+import { switchMap, tap } from 'rxjs/operators';
+import { CommentsService } from './comments.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Observable } from 'rxjs';
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Comments } from './comment';
 
 @Component({
@@ -8,8 +11,32 @@ import { Comments } from './comment';
   styleUrls: ['./comments.component.css'],
 })
 export class CommentsComponent implements OnInit {
-  comments$!: Observable<Comments>;
-  constructor() {}
+  @Input()
+  id!: number;
 
-  ngOnInit(): void {}
+  comments$!: Observable<Comments>;
+  commentForm!: FormGroup;
+
+  constructor(
+    private commentsService: CommentsService,
+    private formBuilder: FormBuilder
+  ) {}
+
+  ngOnInit(): void {
+    this.comments$ = this.commentsService.findComment(this.id);
+    this.commentForm = this.formBuilder.group({
+      comment: ['', Validators.maxLength(300)],
+    });
+  }
+
+  save(): void {
+    const comment = this.commentForm.get('comment')?.value ?? '';
+    this.comments$ = this.commentsService.includeComment(this.id, comment).pipe(
+      switchMap(() => this.commentsService.findComment(this.id)),
+      tap(() => {
+        this.commentForm.reset();
+        alert('Comentário salvo');
+      })
+    );
+  }
 }
