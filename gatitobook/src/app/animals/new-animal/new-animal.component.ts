4ba4370127d6ff7f3f7eced678680f99c72a19e4
Genilton Souza <genilton.souza26@gmail.com>
2021-08-12 11:56:05 -0400

    05 Novo Animal- Aula 07: Componente novo animal
    Curso Angular: Controle o fluxo de navegação. Módulo 5

--- gatitobook/src/app/animals/new-animal/new-animal.component.ts
+++ gatitobook/src/app/animals/new-animal/new-animal.component.ts
@@ -1,5 +1,9 @@
-import { FormGroup } from '@angular/forms';
+import { AnimalsService } from './../animals.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { finalize } from 'rxjs/operators';
+import { HttpEvent, HttpEventType } from '@angular/common/http';
 
 @Component({
   selector: 'app-new-animal',
@@ -12,13 +16,48 @@ export class NewAnimalComponent implements OnInit {
   preview!: string;
   uploadComplete = 0;
 
-  constructor() {}
+  constructor(
+    private animalsService: AnimalsService,
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) {}
 
-  ngOnInit(): void {}
-
-  upload() {
-    console.log('foo');
+  ngOnInit(): void {
+    this.animalForm = this.formBuilder.group({
+      file: ['', Validators.required],
+      description: ['', Validators.maxLength(300)],
+      allowComments: [true],
+    });
   }
 
-  saveFile(file: any) {}
+  upload() {
+    const allowComments = this.animalForm.get('allowComments')?.value ?? false;
+    const description = this.animalForm.get('description')?.value ?? '';
+    this.animalsService
+      .upload(description, allowComments, this.file)
+      .pipe(
+        finalize(() => {
+          this.router.navigate(['animais']);
+        })
+      )
+      .subscribe(
+        (event: HttpEvent<any>) => {
+          if (event.type == HttpEventType.UploadProgress) {
+            const total = event.total ?? 1;
+            this.uploadComplete = Math.round(100 * (event.loaded / total));
+          }
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
+  }
+
+  saveFile(file: any): void {
+    const [fileToSave] = file?.files;
+    this.file = fileToSave;
+    const reader = new FileReader();
+    reader.onload = (event: any) => (this.preview = event.target.result);
+    reader.readAsDataURL(fileToSave);
+  }
 }
