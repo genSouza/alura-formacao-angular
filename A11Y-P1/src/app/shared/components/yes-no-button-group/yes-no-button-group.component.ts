dcc92c1dc36403fa83daa67c78a003bc2eb4daa1
Genilton Souza <genilton.souza26@gmail.com>
2021-08-24 12:09:36 -0400

    01 Prepararando o terrno, primeiro componente - Aula 01: Componente
    Curso Angular: Design de componentes com acessibilidade. Módulo 1

---
+++ A11Y-P1/src/app/shared/components/yes-no-button-group/yes-no-button-group.component.ts
@@ -0,0 +1,31 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-yes-no-button-group',
+  templateUrl: './yes-no-button-group.component.html',
+  styleUrls: ['./yes-no-button-group.component.scss'],
+})
+export class YesNoButtonGroupComponent implements OnInit {
+  /**
+   *
+   */
+  @Input()
+  public value: string = null;
+
+  /**
+   *
+   */
+  @Input()
+  public label = '';
+
+  /**
+   * @internal
+   */
+  constructor() {}
+
+  /**
+   * @internal
+   */
+  ngOnInit(): void {}
+}
