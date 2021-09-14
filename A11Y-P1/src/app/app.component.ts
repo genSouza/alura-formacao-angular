abd8805e025501f25838bf939e34b534599e06ee
Genilton Souza <genilton.souza26@gmail.com>
2021-09-14 09:34:14 -0400

    02 Integração com Reactive Forms- Aula 02: Criando um formulário
    Curso Angular: Design de componentes com acessibilidade. Módulo 2

--- A11Y-P1/src/app/app.component.ts
+++ A11Y-P1/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  public yesNoAnswer: string = 'no';
+  public formGroup: FormGroup = null;
   title = 'a11y-p1';
+
+  /**
+   *@internal
+   */
+  constructor(formBuilder: FormBuilder) {
+    this.formGroup = formBuilder.group({
+      yesNoAnswer: ['no'],
+    });
+  }
+
+  public submit(): void {
+    console.log(this.formGroup.value);
+  }
 }
