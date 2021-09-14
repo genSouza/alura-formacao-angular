09996d720913cfa0e22f7b87eb99ff3731dcb6c2
Genilton Souza <genilton.souza26@gmail.com>
2021-09-14 09:48:17 -0400

    02 Integração com Reactive Forms- Aula 04: A interface ControlValueAcessor
    Curso Angular: Design de componentes com acessibilidade. Módulo 2

--- A11Y-P1/src/app/shared/components/yes-no-button-group/yes-no-button-group.component.ts
+++ A11Y-P1/src/app/shared/components/yes-no-button-group/yes-no-button-group.component.ts
@@ -1,11 +1,12 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ControlValueAccessor } from '@angular/forms';
 
 @Component({
   selector: 'app-yes-no-button-group',
   templateUrl: './yes-no-button-group.component.html',
   styleUrls: ['./yes-no-button-group.component.scss'],
 })
-export class YesNoButtonGroupComponent implements OnInit {
+export class YesNoButtonGroupComponent implements OnInit, ControlValueAccessor {
   /**
    *
    */
@@ -28,11 +29,32 @@ export class YesNoButtonGroupComponent implements OnInit {
    * Component default options
    */
 
-  options = YesNoButtonGroupOptions;
+  public options = YesNoButtonGroupOptions;
+
+  public onChange = (value: string) => {};
+  public onTouched = () => {};
+
   /**
    * @internal
    */
   constructor() {}
+
+  public writeValue(value: any): void {
+    this.value = value;
+    this.onChange(this.value);
+  }
+
+  public registerOnChange(fn: (value: string) => void): void {
+    this.onChange = fn;
+  }
+
+  public registerOnTouched(fn: () => void): void {
+    this.onTouched = fn;
+  }
+
+  public setDisabledState?(isDisabled: boolean): void {
+    throw new Error('Method not implemented.');
+  }
 
   /**
    * @internal
