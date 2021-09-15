b32669e7f83ff6ee1bd8f54ddbec0bc1e2819db5
Genilton Souza <genilton.souza26@gmail.com>
2021-09-15 17:19:34 -0400

    02 Integração com Reactive Forms- Aula 06: NG_VALUE_ACCESSOR
    Curso Angular: Design de componentes com acessibilidade. Módulo 2

--- A11Y-P1/src/app/shared/components/yes-no-button-group/yes-no-button-group.component.ts
+++ A11Y-P1/src/app/shared/components/yes-no-button-group/yes-no-button-group.component.ts
@@ -1,10 +1,24 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { ControlValueAccessor } from '@angular/forms';
+import {
+  Component,
+  EventEmitter,
+  forwardRef,
+  Input,
+  OnInit,
+  Output,
+} from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'app-yes-no-button-group',
   templateUrl: './yes-no-button-group.component.html',
   styleUrls: ['./yes-no-button-group.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      multi: true,
+      useExisting: forwardRef(() => YesNoButtonGroupComponent),
+    },
+  ],
 })
 export class YesNoButtonGroupComponent implements OnInit, ControlValueAccessor {
   /**
@@ -39,9 +53,15 @@ export class YesNoButtonGroupComponent implements OnInit, ControlValueAccessor {
    */
   constructor() {}
 
+  /**
+   * @internal
+   */
+  ngOnInit(): void {}
+
   public writeValue(value: any): void {
     this.value = value;
     this.onChange(this.value);
+    this.valueChange.emit(this.value);
   }
 
   public registerOnChange(fn: (value: string) => void): void {
@@ -56,14 +76,8 @@ export class YesNoButtonGroupComponent implements OnInit, ControlValueAccessor {
     throw new Error('Method not implemented.');
   }
 
-  /**
-   * @internal
-   */
-  ngOnInit(): void {}
-
   public activate(value: string): void {
-    this.value = value;
-    this.valueChange.emit(this.value);
+    this.writeValue(value);
   }
 }
 
