28610c90319703931754e16fa676c3e0d4076525
Genilton Souza <genilton.souza26@gmail.com>
2021-09-21 09:52:44 -0400

    04 Lidando com o teclado- Aula 08: Keyboard managedItem directive
    Curso Angular: Design de componentes com acessibilidade. Módulo 4

--- A11Y-P1/src/app/shared/directives/keyboard-manager/keyboard-manager.directive.ts
+++ A11Y-P1/src/app/shared/directives/keyboard-manager/keyboard-manager.directive.ts
@@ -1,9 +1,17 @@
-import { Directive, HostListener } from '@angular/core';
+import { KeyboardManagedItemDirective } from './keyboard-managed-item.directive';
+import {
+  ContentChildren,
+  Directive,
+  HostListener,
+  QueryList,
+} from '@angular/core';
 
 @Directive({
   selector: '[appKm]',
 })
 export class KeyboardManagerDirective {
+  @ContentChildren(KeyboardManagedItemDirective)
+  public items: QueryList<KeyboardManagedItemDirective> = null;
   @HostListener('keyup', ['$event'])
   public manageKeys(event: KeyboardEvent): void {
     switch (event.key) {
