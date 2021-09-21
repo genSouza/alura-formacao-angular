a9bbecd3cceb9681e4f9c23734e845a716286ee4
Genilton Souza <genilton.souza26@gmail.com>
2021-09-21 13:40:22 -0400

    05 Revisando o nosso componente- Aula 04: Disable control Directive
    Curso Angular: Design de componentes com acessibilidade. Módulo 5

---
+++ A11Y-P1/src/app/shared/directives/disable-control/disable-control.directive.ts
@@ -0,0 +1,25 @@
+import { Directive, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { NgControl } from '@angular/forms';
+
+@Directive({
+  selector: '[appDisableControl]',
+})
+export class DisableControlDirective implements OnChanges {
+  /**
+   * input property
+   */
+  @Input()
+  appDisableControl = false;
+
+  /**
+   *@internal
+   */
+  constructor(private ngControl: NgControl) {}
+
+  public ngOnChanges(changes: SimpleChanges): void {
+    if (changes.appDisableControl) {
+      const action = this.appDisableControl ? 'disable' : 'enable';
+      this.ngControl.control[action]();
+    }
+  }
+}
