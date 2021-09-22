6923b654c885aac5c745303a3386109f130fd6b5
Genilton Souza <genilton.souza26@gmail.com>
2021-09-22 15:31:53 -0400

    02 Lapidando nossos testes-Aula 03: Elegância dos testes
    Curso: Angular: Teste automatizados com Jasmine e Karma. Módulo 2

--- ng-test1/src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ ng-test1/src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -5,4 +5,13 @@ describe(UniqueIdService.name, () => {
     const id = service.generateUniqueIdWithPrefix('app');
     expect(id.startsWith('app-')).toBeTrue();
   });
+
+  it(`${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should not generate duplicate id's when called multiple times`, () => {
+    const service = new UniqueIdService();
+    const ids = new Set();
+    for (let i = 0; i < 50; i++) {
+      ids.add(service.generateUniqueIdWithPrefix('app'));
+    }
+    expect(ids.size).toBe(50);
+  });
 });
