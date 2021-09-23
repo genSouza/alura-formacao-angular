b3476c9122726f95a51fa3e9c1596d8b0195aaed
Genilton Souza <genilton.souza26@gmail.com>
2021-09-22 22:38:18 -0400

    02 Lapidando nossos testes-Aula 07: Testando exceções
    Curso: Angular: Teste automatizados com Jasmine e Karma. Módulo 2

--- ng-test1/src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ ng-test1/src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -5,12 +5,14 @@ describe(UniqueIdService.name, () => {
     service = new UniqueIdService();
   });
 
-  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should generate id when called with prefix`, () => {
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name}
+    should generate id when called with prefix`, () => {
     const id = service.generateUniqueIdWithPrefix('app');
     expect(id.startsWith('app-')).toBeTrue();
   });
 
-  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should not generate duplicate id's when called multiple times`, () => {
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name}
+    should not generate duplicate id's when called multiple times`, () => {
     const ids = new Set();
     for (let i = 0; i < 50; i++) {
       ids.add(service.generateUniqueIdWithPrefix('app'));
@@ -18,9 +20,18 @@ describe(UniqueIdService.name, () => {
     expect(ids.size).toBe(50);
   });
 
-  it(`#${UniqueIdService.prototype.getNumberOfGeneratedUniqueIds.name} should return the number of generated id's when called`, () => {
+  it(`#${UniqueIdService.prototype.getNumberOfGeneratedUniqueIds.name}
+    should return the number of generated id's when called`, () => {
     service.generateUniqueIdWithPrefix('app');
     service.generateUniqueIdWithPrefix('app');
     expect(service.getNumberOfGeneratedUniqueIds()).toBe(2);
+  });
+
+  it(`#${UniqueIdService.prototype.getNumberOfGeneratedUniqueIds.name}
+    should throw error when called empty`, () => {
+    const emptyValues = [null, undefined, ''];
+    emptyValues.forEach((emptyValue) => {
+      expect(() => service.generateUniqueIdWithPrefix(emptyValue)).toThrow();
+    });
   });
 });
