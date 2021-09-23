0a8a19ccf111a1b90c9721cff2b838b4663d0983
Genilton Souza <genilton.souza26@gmail.com>
2021-09-23 16:16:36 -0400

    04 Testando componentes-Aula 03: O papel do TestBed
    Curso: Angular: Teste automatizados com Jasmine e Karma. Módulo 4

---
+++ ng-test1/src/app/shared/components/like-widget/like-widget.component.specs.ts
@@ -0,0 +1,15 @@
+import { TestBed } from '@angular/core/testing';
+import { UniqueIdService } from '../../services/unique-id/unique-id.service';
+import { LikeWidgetComponent } from './like-widget.component';
+
+describe(`#${LikeWidgetComponent.name}`, () => {
+  let component: LikeWidgetComponent = null;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [LikeWidgetComponent],
+    }).compileComponents();
+  });
+
+  it('', () => {});
+});
