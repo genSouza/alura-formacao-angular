f3f301681f084bddd86bee08bba8297df20dbc15
Genilton Souza <genilton.souza26@gmail.com>
2021-09-23 15:24:09 -0400

    03 O componente LikeWidget-Aula 05: Preview do nosso componente
    Curso: Angular: Teste automatizados com Jasmine e Karma. Módulo 3

--- ng-test1/src/app/shared/components/like-widget/like-widget.component.ts
+++ ng-test1/src/app/shared/components/like-widget/like-widget.component.ts
@@ -4,7 +4,7 @@ import { faThumbsUp } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-like-widget',
-  template: './like-widget.component.html',
+  templateUrl: './like-widget.component.html',
   styleUrls: ['./like-widget.component.scss'],
 })
 export class LikeWidgetComponent implements OnInit {
@@ -34,5 +34,9 @@ export class LikeWidgetComponent implements OnInit {
     if (!this.id) {
       this.id = this.uniqueIdService.generateUniqueIdWithPrefix('like-widget');
     }
+  }
+
+  public like(): void {
+    this.liked.emit();
   }
 }
--- ng-test1/src/app/shared/components/like-widget/like-widget.module.ts
+++ ng-test1/src/app/shared/components/like-widget/like-widget.module.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { UniqueIdService } from '../../services/unique-id/unique-id.service';
+import { LikeWidgetComponent } from './like-widget.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [LikeWidgetComponent],
   imports: [CommonModule, FontAwesomeModule],
-  exports: [],
+  exports: [LikeWidgetComponent],
   providers: [UniqueIdService],
 })
 export class LikeWidgetModule {}
