5f8cddaa18f2721be859fc6c960011d64bda2067
Genilton Souza <genilton.souza26@gmail.com>
2021-10-01 09:58:09 -0400

    01 Novo componente, nova complexidade- Aula 05: O componente photoframe
    Curso: Angular: Avançando com testes autmatizados. Módulo 1

--- ng-test2/src/app/shared/components/photo-frame/photo-frame.component.ts
+++ ng-test2/src/app/shared/components/photo-frame/photo-frame.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-photo-frame',
   templateUrl: './photo-frame.component.html',
-  styleUrls: ['./photo-frame.component.scss']
+  styleUrls: ['./photo-frame.component.scss'],
 })
 export class PhotoFrameComponent {
+  /**
+   *
+   */
+  @Input()
+  description = '';
 
+  /**
+   *
+   */
+  @Input()
+  src = '';
+
+  /**
+   *
+   */
+  @Input()
+  likes = 0;
+
+  /**
+   *
+   */
+  @Output()
+  liked: EventEmitter<void> = new EventEmitter();
+
+  public like(): void {
+    this.liked.emit();
+  }
 }
