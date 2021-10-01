f7925989221d6a38344db9078c5db9a17eceef6a
Genilton Souza <genilton.souza26@gmail.com>
2021-10-01 11:43:31 -0400

    01 Novo componente, nova complexidade- Aula 08: Revisando conceitos e primeiro teste
    Curso: Angular: Avançando com testes autmatizados. Módulo 1

--- ng-test2/src/app/shared/components/photo-frame/photo-frame.component.ts
+++ ng-test2/src/app/shared/components/photo-frame/photo-frame.component.ts
@@ -1,11 +1,38 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  OnInit,
+  OnDestroy,
+} from '@angular/core';
+import { Subject } from 'rxjs';
+import { debounceTime, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-photo-frame',
   templateUrl: './photo-frame.component.html',
   styleUrls: ['./photo-frame.component.scss'],
 })
-export class PhotoFrameComponent {
+export class PhotoFrameComponent implements OnInit, OnDestroy {
+  private debounceSubject: Subject<void> = new Subject();
+  private unsubscribe: Subject<void> = new Subject();
+
+  ngOnInit(): void {
+    this.debounceSubject
+      .asObservable()
+      .pipe(debounceTime(500))
+      .pipe(takeUntil(this.unsubscribe))
+      .subscribe(() => {
+        this.liked.emit();
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe.next();
+    this.unsubscribe.complete();
+  }
+
   /**
    *
    */
@@ -31,6 +58,6 @@ export class PhotoFrameComponent {
   liked: EventEmitter<void> = new EventEmitter();
 
   public like(): void {
-    this.liked.emit();
+    this.debounceSubject.next();
   }
 }
