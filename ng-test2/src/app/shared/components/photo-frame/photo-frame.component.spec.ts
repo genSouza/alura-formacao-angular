3175e55b2e2da4e6d321503e791360c224b60876
Genilton Souza <genilton.souza26@gmail.com>
2021-10-07 14:24:46 -0400

    03 Lidando com eventos de UI- Aula04: Simulando cliques
    Curso: Angular: Avançando com testes autmatizados. Módulo 3

--- ng-test2/src/app/shared/components/photo-frame/photo-frame.component.spec.ts
+++ ng-test2/src/app/shared/components/photo-frame/photo-frame.component.spec.ts
@@ -1,4 +1,3 @@
-import { element } from 'protractor';
 import { PhotoFrameModule } from './photo-frame.module';
 import {
   ComponentFixture,
@@ -80,5 +79,20 @@ describe(PhotoFrameComponent.name, () => {
     const img: HTMLImageElement = fixture.nativeElement.querySelector('img');
     expect(img.getAttribute('src')).toBe(src);
     expect(img.getAttribute('alt')).toBe(description);
+  });
+
+  it(`(D) Should display number of likes when clicked`, (done) => {
+    fixture.detectChanges();
+    component.liked.subscribe(() => {
+      component.likes++;
+      fixture.detectChanges();
+      const counterEl: HTMLElement =
+        fixture.nativeElement.querySelector('.like-counter');
+      expect(counterEl.textContent.trim()).toBe('1');
+      done();
+    });
+    const likeWidgetContainerEl: HTMLElement =
+      fixture.nativeElement.querySelector('.like-widget-container');
+    likeWidgetContainerEl.click();
   });
 });
