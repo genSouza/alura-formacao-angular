322b549d61dc0f2663bf38645a3563321da11a79
Genilton Souza <genilton.souza26@gmail.com>
2021-10-11 17:04:31 -0400

    05 Avançando nos testes- Aula 02: Botando a casa em ordem
    Curso: Angular: Avançando com testes automatizados. Módulo 5

--- ng-test2/src/app/app.component.ts
+++ ng-test2/src/app/app.component.ts
@@ -1,26 +1,11 @@
-import { PhotoBoardService } from './shared/components/photo-board/services/photo-board.service';
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import { Photo } from './shared/components/photo-board/interfaces/photo';
-import { take } from 'rxjs/operators';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'Angular testing';
-  public photos: Photo[] = [];
 
-  constructor(private service: PhotoBoardService) {}
-
-  ngOnInit(): void {
-    this.service
-      .getPhotos()
-      .pipe(take(1))
-      .subscribe((res) => {
-        this.photos = res;
-      });
-  }
 }
