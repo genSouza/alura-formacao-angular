a5e8e9f88e5e71d886cd33f3fc168bdf60d59536
Genilton Souza <genilton.souza26@gmail.com>
2021-10-11 17:20:23 -0400

    05 Avançando nos testes- Aula 03: Loading spinner
    Curso: Angular: Avançando com testes automatizados. Módulo 5

--- ng-test2/src/app/components/photo-list/photo-list.component.ts
+++ ng-test2/src/app/components/photo-list/photo-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faCircleNotch } from '@fortawesome/free-solid-svg-icons';
 import { take } from 'rxjs/operators';
 import { Photo } from 'src/app/shared/components/photo-board/interfaces/photo';
 import { PhotoBoardService } from 'src/app/shared/components/photo-board/services/photo-board.service';
@@ -6,12 +7,13 @@ import { PhotoBoardService } from 'src/app/shared/components/photo-board/service
 @Component({
   selector: 'app-photo-list',
   templateUrl: './photo-list.component.html',
-  styleUrls: ['./photo-list.component.scss']
+  styleUrls: ['./photo-list.component.scss'],
 })
 export class PhotoListComponent implements OnInit {
-
   public photos: Photo[] = [];
-
+  public fa = {
+    faCircleNotch,
+  };
   constructor(private service: PhotoBoardService) {}
 
   ngOnInit(): void {
