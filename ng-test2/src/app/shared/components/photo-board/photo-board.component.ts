942f7559c71d3482b5776af700cbb2ee7b47ad51
Genilton Souza <genilton.souza26@gmail.com>
2021-10-11 11:33:41 -0400

    04 Cuidados com a abstração do Angular- Aula 02: O componente photoboard
    Curso: Angular: Avançando com testes autmatizados. Módulo 4

---
+++ ng-test2/src/app/shared/components/photo-board/photo-board.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+import { Photo } from './interfaces/photo';
+
+@Component({
+  selector: 'app-photo-board',
+  templateUrl: './photo-board.component.html',
+  styleUrls: ['photo-board.component.scss'],
+})
+export class PhotoBoardComponent {
+  @Input()
+  public photos: Photo[];
+}
