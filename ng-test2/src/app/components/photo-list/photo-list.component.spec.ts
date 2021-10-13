1b408cd95892e12b13755b55ea4fb9283d701182
Genilton Souza <genilton.souza26@gmail.com>
2021-10-13 18:07:36 -0400

    05 Avançando nos testes- Aula 09: Finalizando o teste
    Curso: Angular: Avançando com testes automatizados. Módulo 5

--- ng-test2/src/app/components/photo-list/photo-list.component.spec.ts
+++ ng-test2/src/app/components/photo-list/photo-list.component.spec.ts
@@ -3,6 +3,8 @@ import { PhotoListModule } from './photo-list.module';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { PhotoListComponent } from './photo-list.component';
 import { HttpClientModule } from '@angular/common/http';
+import { buildPhotoList } from 'src/app/shared/components/photo-board/test/build-photo-list';
+import { of } from 'rxjs';
 
 describe(PhotoListComponent.name, () => {
   let fixture: ComponentFixture<PhotoListComponent>;
@@ -24,7 +26,26 @@ describe(PhotoListComponent.name, () => {
   });
 
   it('(D) Should display board when data arrives', () => {
+    const photos = buildPhotoList();
+    spyOn(service, 'getPhotos').and.returnValue(of(photos));
+    fixture.detectChanges();
+    const board = fixture.nativeElement.querySelector('app-photo-board');
+    const loader = fixture.nativeElement.querySelector('.loader');
+
+    expect(board).not.toBeNull();
+    expect(loader).toBeNull();
+  });
+
+  it('(D) Should display loader while waiting for data', () => {
+    const photos = buildPhotoList();
+    //spyOn(service, 'getPhotos').and.returnValue(null);
     fixture.detectChanges();
 
+    const board = fixture.nativeElement.querySelector('app-photo-board');
+    const loader = fixture.nativeElement.querySelector('.loader');
+
+    expect(board).toBeNull();
+    expect(loader).not.toBeNull();
   });
+
 });
