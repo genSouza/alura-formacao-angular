f32cbd35c53968aef3acf7de2ffcc381722c3791
Genilton Souza <genilton.souza26@gmail.com>
2021-10-14 18:45:36 -0400

    02 Angular e RxJS- Aula 04: Unsubscribe
    Curso: Angular e RxJs: Programação reativa. Módulo 2.

--- angular_rxjs/byte-bank-broker/src/app/acoes/acoes.service.ts
+++ angular_rxjs/byte-bank-broker/src/app/acoes/acoes.service.ts
@@ -1,15 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { map, pluck, tap } from 'rxjs/operators';
-import { Acao, Acoes } from './modelo/acoes';
+import { Acao, Acoes, AcoesAPI } from './modelo/acoes';
 @Injectable({
   providedIn: 'root',
 })
 export class AcoesService {
   constructor(private httpClient: HttpClient) {}
 
-  getAcoes(): any {
-    return this.httpClient.get<any>('http://localhost:3000/acoes').pipe(
+  getAcoes(): Observable<Acoes>{
+    return this.httpClient.get<AcoesAPI>('http://localhost:3000/acoes').pipe(
       tap((data) => console.log(data)),
       //map((api) => api.payload),
       pluck('payload'),
